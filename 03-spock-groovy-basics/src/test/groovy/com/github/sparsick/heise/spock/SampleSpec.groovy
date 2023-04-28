a42378fc7e70a83ce07f7de8371a2a5574144872
Sandra Parsick <mail@sandra-parsick.de>
2023-04-28 08:32:26 +0200

    spock and groovy basics

--- 03-spock-groovy-basics/src/test/groovy/com/github/sparsick/heise/spock/SampleSpec.groovy
+++ 03-spock-groovy-basics/src/test/groovy/com/github/sparsick/heise/spock/SampleSpec.groovy
@@ -1,9 +1,41 @@
 package com.github.sparsick.heise.spock
 
+import spock.lang.Shared
 import spock.lang.Specification
 
 class SampleSpec extends Specification {
 
+    @Shared
+    def person
 
+    def setup() {
+        person = new Person("Tom", "Taylor", " Software Developer")
+    }
+
+    def cleanup() {
+
+    }
+
+    def setupSpec(){
+
+    }
+
+    def cleanupSpec(){
+
+    }
+
+    def "Teste Person nach seinem Vornamen"() {
+        when:
+        def firstName = person.firstName
+        then:
+        firstName == "Tom"
+    }
+
+    def "Teste Person nach seinem Nachnamen"() {
+        when:
+        def lastName = person.lastName
+        then:
+        lastName == "Taylor"
+    }
 
 }
