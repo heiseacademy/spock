f093a0093320e264577142b83c9f53afdc821480
Sandra Parsick <mail@sandra-parsick.de>
2023-05-13 15:20:24 +0200

    sample mocking

--- 06-mocking/src/test/groovy/com/github/sparsick/heise/spock/MockingSpec.groovy
+++ 06-mocking/src/test/groovy/com/github/sparsick/heise/spock/MockingSpec.groovy
@@ -5,5 +5,74 @@ import spock.lang.Specification
 
 class MockingSpec extends Specification {
 
+    def "simple interaction" (){
+        given:
+        def serviceMock = Mock(Service)
+        Service serviceMock2 = Mock()
+        def controllerUnderTest = new Controller(serviceMock)
+
+        when:
+        controllerUnderTest.handleRequest("Hello World")
+
+        then:
+        1 * serviceMock.receiveData("Hello World")
+        1 * serviceMock.letMeKnow()
+    }
+
+    def "order is important" (){
+        given:
+        def serviceMock = Mock(Service)
+        Service serviceMock2 = Mock()
+        def controllerUnderTest = new Controller(serviceMock)
+
+        when:
+        controllerUnderTest.handleRequest("Hello World")
+
+        then:
+        1 * serviceMock.letMeKnow()
+
+        then:
+        1 * serviceMock.receiveData("Hello World")
+    }
+
+    def "focus on target object" (){
+        given:
+        def serviceMock = Mock(Service)
+        def controllerUnderTest = new Controller(serviceMock)
+
+        when:
+        controllerUnderTest.handleRequest("Hello World")
+
+        then:
+        2 * serviceMock._
+    }
+
+    def "focus on method" (){
+        given:
+        def serviceMock = Mock(Service)
+        def controllerUnderTest = new Controller(serviceMock)
+
+        when:
+        controllerUnderTest.handleRequest(null)
+
+        then:
+        1 * serviceMock.letMeKnow()
+        1 * serviceMock.receiveData(_)
+    }
+
+    def "mixing interaction and condition" (){
+        given:
+        def serviceMock = Mock(Service)
+        def controllerUnderTest = new Controller(serviceMock)
+
+        when:
+        def result =controllerUnderTest.handleRequest(null)
+
+        then:
+        1 * serviceMock.letMeKnow()
+        1 * serviceMock.receiveData(_)
+        result == false
+    }
+
 
 }
--- 06-mocking/src/test/groovy/com/github/sparsick/heise/spock/StubbingSpec.groovy
+++ 06-mocking/src/test/groovy/com/github/sparsick/heise/spock/StubbingSpec.groovy
@@ -4,6 +4,61 @@ import spock.lang.Specification
 
 class StubbingSpec extends Specification {
 
+    def "simple stubbing" (){
+        given:
+        def serviceMock = Mock(Service)
+        def controllerUnderTest = new Controller(serviceMock)
+
+        when:
+        def result =controllerUnderTest.handleRequest(null)
+
+        then:
+        serviceMock.receiveData(_) >> true
+        result == true
+    }
+
+    def "several mock calls" (){
+        given:
+        def serviceMock = Mock(Service)
+
+        when:
+        println(serviceMock.receiveData(""))
+        println(serviceMock.receiveData(""))
+        println(serviceMock.receiveData(""))
+
+        then:
+        serviceMock.receiveData(_) >>> [true, false, false]
+    }
+
+    def "computing mock calls" (){
+        given:
+        def serviceMock = Mock(Service)
+
+        when:
+        println(serviceMock.receiveData("12"))
+        println(serviceMock.receiveData(""))
+        println(serviceMock.receiveData("123"))
+
+        then:
+        serviceMock.receiveData(_) >> {String data -> data.length() == 0}
+    }
+
+    def "mixing stubbing and mocking"() {
+        given:
+        def serviceMock = Mock(Service)
+        def controllerUnderTest = new Controller(serviceMock)
+
+        when:
+        def result = controllerUnderTest.handleRequest("Hello World")
+
+        then:
+        1 * serviceMock.letMeKnow()
+        1 * serviceMock.receiveData(_) >> true
+        result == true
+    }
+
+
+
 
 
 }
